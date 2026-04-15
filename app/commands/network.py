e6cdeac54868f817a15140720b4b79b947585d5f
ES_PREPA_DEV <sosoros66@icloud.com>
2026-04-15 12:14:40 +0200

    begin network

---
+++ app/commands/network.py
@@ -0,0 +1,60 @@
+import platform
+import subprocess
+import psutil
+from config import Config 
+
+def ping_command(cmd: str) -> str:
+    host = cmd.split(" ")[1] 
+    param = "-n" if platform.system().lower() == "windows" else "-c"
+    encoding = "cp850" if platform.system().lower() == "windows" else "utf-8"
+    result = subprocess.run(
+    ["ping", param, str(Config.PING_COUNT), host],
+    capture_output=True,
+    test=True,
+    encoding=encoding
+    )   
+    return result.stdout
+
+def network_command() -> str:
+    net_io = psutil.net_io_counters()
+    addrs = psutil.net_if_addrs()
+    result = "🌐 Informations réseau :\n\n"
+    result += f"📶​ Paquets reçus: {net_io.packets_recv }\n"
+    result += f"📶​ Paquets envoyés: {net_io.packets_sent }\n"
+    result += f" Donnees envoyees: {net_io.bytes_sent // (2**20)}MO\n"
+    result += f" Donnees reçues: {net_io.bytes_recv // (2**20)}MO\n\n"
+    
+    result += "🔌 Interfaces réseau :\n"
+    for interface, addr_list in addrs.items():
+        result += f"\n{interface}:\n"
+        for addr in addr_list:
+            if addr.family == 2:
+                result += f"  🌐 IPv4: {addr.address}\n"
+            elif addr.family == 23:
+                result += f"  🌐 IPv6: {addr.address}\n"
+    return result
+
+
+def ports_command() -> str:
+    connections = psutil.net_connections(kind='inet')
+    listening = [conn for conn in connections if conn.status == "LISTEN"]
+    result = "🔌 Ports actifs :\n\n"
+    result += f"{'protocole:':<10} {'adress Locale ':<25} {'PID':<10} {'Programme'}\n"
+    result += "-"* 80 + "\n"
+    
+    seen_ports = set()
+    
+    for conn in listening:
+        if conn.laddr.port in seen_ports:
+            continue
+        seen_ports.add(conn.laddr.port)
+        try:
+            proc = psutil.Process(conn.pid) if conn.pid else None
+            proc_name = proc.name() if proc else "N/A"
+        except Exception:
+            proc_name = "N/A"
+        proto = "TCP" if conn.type == 1 else "UDP"
+        addr = f"{conn.laddr.ip}:{conn.laddr.port}"
+        result += f"{proto:<10} {addr:<25} {str(conn.pid or 'N/A'):<10} {proc_name}\n"
+    return result
+
